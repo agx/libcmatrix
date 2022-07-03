e2f2ec55eef87b5c19a9eb7cdf8d2b4a656f0d4d
Mohammed Sadiq <sadiq@sadiqpk.org>
2022-07-03 17:14:40 +0530

    tests: Add room-member test

---
+++ tests/room-member.c
@@ -0,0 +1,51 @@
+/* -*- mode: c; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+/* room-member.c
+ *
+ * Copyright 2022 Purism SPC
+ *
+ * Author(s):
+ *   Mohammed Sadiq <sadiq@sadiqpk.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#undef NDEBUG
+#undef G_DISABLE_ASSERT
+#undef G_DISABLE_CHECKS
+#undef G_DISABLE_CAST_CHECKS
+#undef G_LOG_DOMAIN
+
+#include "cm-room-private.h"
+#include "users/cm-room-member.c"
+
+static void
+test_room_member_new (void)
+{
+  CmRoomMember *member;
+  CmClient *client;
+  CmRoom *room;
+  CmUser *user;
+
+  room = cm_room_new ("random room");
+  client = cm_client_new ();
+  member = cm_room_member_new (room, client, "@alice:example.co");
+  user = CM_USER (member);
+  g_object_unref (room);
+  g_object_unref (client);
+  g_assert (CM_IS_ROOM_MEMBER (member));
+
+  g_assert_cmpstr (cm_user_get_id (user), ==, "@alice:example.co");
+
+  g_assert_finalize_object (room);
+}
+
+int
+main (int   argc,
+      char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/room-member/new", test_room_member_new);
+
+  return g_test_run ();
+}
