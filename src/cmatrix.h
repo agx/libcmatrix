204397fd071432d893ad9744ea77ce65b418e42f
Guido Günther <agx@sigxcpu.org>
2024-07-15 16:37:51 +0200

    build: Make room-event public
    It's the parent class of room-message-event

--- src/cmatrix.h
+++ src/cmatrix.h
@@ -30,6 +30,7 @@ G_BEGIN_DECLS
 #include "cm-pusher.h"
 #include "cm-room.h"
 #include "events/cm-event.h"
+#include "events/cm-room-event.h"
 #include "events/cm-room-message-event.h"
 #include "events/cm-verification-event.h"
 /* these are not yet public */
