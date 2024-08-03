0f8fa78367065d998269c952229e680df276c9a4
Evangelos Ribeiro Tzaras <devrtz@fortysixandtwo.eu>
2024-08-03 19:01:01 +0200

    client-private: Reindent header
    Gbp-Dch: Ignore

--- src/cm-client-private.h
+++ src/cm-client-private.h
@@ -46,14 +46,14 @@ const char *cm_client_get_filter_id               (CmClient            *self);
 void        cm_client_save                        (CmClient            *self);
 const char *cm_client_get_next_batch              (CmClient            *self);
 CmUserList *cm_client_get_user_list               (CmClient            *self);
-void          cm_client_get_file_async                (CmClient              *self,
-                                                       const char            *uri,
-                                                       GCancellable          *cancellable,
-                                                       GFileProgressCallback  progress_callback,
-                                                       gpointer               progress_user_data,
-                                                       GAsyncReadyCallback    callback,
-                                                       gpointer               user_data);
-GInputStream *cm_client_get_file_finish               (CmClient              *self,
-                                                       GAsyncResult          *result,
-                                                       GError               **error);
+void        cm_client_get_file_async              (CmClient              *self,
+                                                   const char            *uri,
+                                                   GCancellable          *cancellable,
+                                                   GFileProgressCallback  progress_callback,
+                                                   gpointer               progress_user_data,
+                                                   GAsyncReadyCallback    callback,
+                                                   gpointer               user_data);
+GInputStream *cm_client_get_file_finish           (CmClient              *self,
+                                                   GAsyncResult          *result,
+                                                   GError               **error);
 G_END_DECLS
